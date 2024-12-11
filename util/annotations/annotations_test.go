ac5523ad4a7ad5ce732cd893eb51c746e72809e0
Jeanette Tan <jeanette.tan@grafana.com>
2024-12-11 14:35:07 +0800

    make annotations use an interface
    Signed-off-by: Jeanette Tan <jeanette.tan@grafana.com>

--- util/annotations/annotations_test.go
+++ util/annotations/annotations_test.go
@@ -26,12 +26,16 @@ func TestAnnotations_AsStrings(t *testing.T) {
 	var annos Annotations
 	pos := posrange.PositionRange{Start: 3, End: 8}
 
-	annos.Add(errors.New("this is a non-annotation error"))
+	annos.AddRaw(errors.New("this is a non-annotation error"))
 
 	annos.Add(NewInvalidRatioWarning(1.1, 100, pos))
 	annos.Add(NewInvalidRatioWarning(1.2, 123, pos))
 
 	annos.Add(NewHistogramIgnoredInAggregationInfo("sum", pos))
+
+	annos.Add(NewHistogramQuantileForcedMonotonicityInfo("series_1", pos, 1735084800000, 5, 50, 5.5))
+	annos.Add(NewHistogramQuantileForcedMonotonicityInfo("series_1", pos, 1703462400000, 10, 100, 10))
+	annos.Add(NewHistogramQuantileForcedMonotonicityInfo("series_1", pos, 1733011200000, 2.5, 75, 7.5))
 
 	warnings, infos := annos.AsStrings("lorem ipsum dolor sit amet", 0, 0)
 	require.ElementsMatch(t, warnings, []string{
@@ -41,5 +45,6 @@ func TestAnnotations_AsStrings(t *testing.T) {
 	})
 	require.ElementsMatch(t, infos, []string{
 		"PromQL info: ignored histogram in sum aggregation (1:4)",
+		`PromQL info: input to histogram_quantile needed to be fixed for monotonicity (see https://prometheus.io/docs/prometheus/latest/querying/functions/#histogram_quantile) for metric name "series_1", from buckets 2.5 to 100, with a max diff of 10, over 3 samples from 2023-12-25T00:00:00Z to 2024-12-25T00:00:00Z (1:4)`,
 	})
 }
